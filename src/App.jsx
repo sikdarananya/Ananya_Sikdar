7dd605a9df91630beda98c16b581622f03d2fb33
Ananya Sikdar <iamananyasikdar@gmail.com>
2024-11-17 20:59:21 +0530

    Hero items added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/hero'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
